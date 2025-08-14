71ba653574262d804e724c50205fdb34f0fca24c
icarillius <icarillius@gmail.com>
2025-08-14 11:53:35 +0100

    Notation Update

--- Source/SubsystemsCPP/TestWorldSubsystem.cpp
+++ Source/SubsystemsCPP/TestWorldSubsystem.cpp
@@ -4,30 +4,31 @@
 #include "TestWorldSubsystem.h"
 
 DEFINE_LOG_CATEGORY(TestWorldSubsystem);
-
+//Debugs on ini
 void UTestWorldSubsystem::Initialize(FSubsystemCollectionBase& Collection)
 {
 	UE_LOGFMT(TestWorldSubsystem, Log, "UTestWorldSubsystem::Initialize");
 }
-
+//Debugs on Deini
 void UTestWorldSubsystem::Deinitialize()
 {
 	UE_LOGFMT(TestWorldSubsystem, Log, "UTestWorldSubsystem::Deinitialize");
 }
-
+//Adds number 1 and 2 together BP editable on function call
 int UTestWorldSubsystem::AddNumbersTogether(int number1, int number2)
 {
 	int number3 = number1 + number2;
 	return number3;
 }
 
+//incriments number 4 and outputs 4.
 int UTestWorldSubsystem::IncreaseNumber()
 {
 	number4++;
 
 	return number4;
 }
-
+//adds 1 to 4 and sets to 5. 
 int UTestWorldSubsystem::UpdateNumber()
 {
 	IncreaseNumber();
--- Source/SubsystemsCPP/TestWorldSubsystem.h
+++ Source/SubsystemsCPP/TestWorldSubsystem.h
@@ -16,16 +16,21 @@ class SUBSYSTEMSCPP_API UTestWorldSubsystem : public UWorldSubsystem
 {
 	GENERATED_BODY()
 	
+	//initializes the code base
 	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+	//Deinitializes the code base
 	virtual void Deinitialize() override;
 
+	//Runs function to add number 1 and 2 together.
 	UFUNCTION(BlueprintCallable)
 	int AddNumbersTogether(int number1, int number2);
+	//Function increase number sent to the increase number widget. 
 
 	UFUNCTION(BlueprintCallable)
 	int IncreaseNumber();
-
+	//Sets the numbers 4 and 5 to int.
 	int number4, number5;
+	//Function Update number sent to the Update number widget. 
 	UFUNCTION(BlueprintCallable)
 	int UpdateNumber();
 	
